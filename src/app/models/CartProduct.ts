a37025d73c1eb1e1620aff828b1dfcf7e6226b50
Patrick Kabuga <kabugap02@gmail.com>
2023-02-16 18:30:06 +0300

    Added a cart product model

---
+++ src/app/models/CartProduct.ts
@@ -0,0 +1,17 @@
+export class CartProduct {
+    id: number;
+    name: string;
+    price: number;
+    url: string;
+    description: string;
+    totalPrice: number;
+
+    constructor () {
+        this.id = 0;
+        this.name = '';
+        this.price = 0;
+        this.url = '';
+        this.description = '';
+        this.totalPrice = 0;
+    }
+}
