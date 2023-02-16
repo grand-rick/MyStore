0fd6f8ea6e85e3be7fb1621d1ef6d3a417ac92fd
Patrick Kabuga <kabugap02@gmail.com>
2023-02-16 16:54:10 +0300

    Added method to remove cart product

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/Product';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartProducts: Product[] = [];
+
+  constructor (private productsService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.cartProducts = this.productsService.getCartProducts();
+  }
+
+  removeCartProduct(product: Product): void {
+    this.cartProducts = this.productsService.removeCartProduct(product);
+    alert(`${product.name} has been removed from cart`);
+  }
 
 }
