dd603b34ca514f86ccc2c7c04af037e5c32ed8fe
Patrick Kabuga <kabugap02@gmail.com>
2023-02-16 18:48:45 +0300

    Removed CartProduct model

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { CartProduct } from 'src/app/models/CartProduct';
 import { Product } from 'src/app/models/Product';
 import { ProductsService } from 'src/app/services/products.service';
 
@@ -18,7 +17,7 @@ export class CartComponent implements OnInit {
     this.cartProducts = this.productsService.getCartProducts();
   }
 
-  removeCartProduct(product: CartProduct): void {
+  removeCartProduct(product: Product): void {
     this.cartProducts = this.productsService.removeCartProduct(product);
     alert(`${product.name} has been removed from cart`);
     alert(`The cart has ${this.cartProducts.length} product remaining, isCartEmpty: ${this.isCartEmpty}`);
