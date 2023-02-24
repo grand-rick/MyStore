675d0e683f804f2fd2227453ac1f880b4a5f5783
Patrick Kabuga <kabugap02@gmail.com>
2023-02-24 13:10:09 +0300

    Added logic to emit remove cart product event

--- src/app/components/cart-input/cart-input.component.ts
+++ src/app/components/cart-input/cart-input.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../models/Product';
+import { ProductsService } from '../../services/products.service';
+
+interface CartP {
+  id: number;
+  amount: number;
+}
 
 @Component({
   selector: 'app-cart-input',
   templateUrl: './cart-input.component.html',
   styleUrls: ['./cart-input.component.css']
 })
-export class CartInputComponent {
 
+export class CartInputComponent implements OnInit {
+  @Input() product: Product;
+  @Output() removeProduct: EventEmitter<Product> = new EventEmitter();
+
+  cartPs: CartP[] = [];
+  amount: number;
+
+  constructor (private productsServices: ProductsService) {
+    this.product = {
+      id: 0,
+      name: '',
+      price: 0,
+      url: '',
+      description: '',
+      amount: 0
+    }
+    this.amount = this.product.amount;
+  }
+
+  ngOnInit(): void {}
+
+  onInputChange(newInput: number): void {
+    this.amount = newInput;
+  }
+
+  removeCartProduct(product: Product) {
+    this.removeProduct.emit(product);
+  }
 }
