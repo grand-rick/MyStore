ea2a857fe255cc271ef1296c2098488dc68fca44
Patrick Kabuga <kabugap02@gmail.com>
2023-02-24 22:26:56 +0300

    Added checkout logic, including redirecting back home after checking out

--- src/app/components/check-out/check-out.component.ts
+++ src/app/components/check-out/check-out.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/Product';
+import { ProductsService } from '../../services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent {
 
+export class CheckOutComponent implements OnInit {
+  totalPrice: number = this.productsService.getTotalPrice();
+  cartProducts: Product[] = this.productsService.getCartProducts();
+
+  constructor (private productsService: ProductsService, private router: Router) {}
+
+  checkOut(): void {
+    alert(`You have checked Out!`);
+    this.goToProductList();
+  }
+
+  ngOnInit(): void {}
+
+  goToProductList() {
+    this.router.navigate(['/']);
+  }
 }
