723e3523788deca06b14097997aabba7de1c6a1a
Patrick Kabuga <kabugap02@gmail.com>
2023-02-24 22:24:05 +0300

    Added routing path to CheckOut component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductItemDetailComponent } from './components/product-item-detail/product-item-detail.component';
+import { CheckOutComponent } from './components/check-out/check-out.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path: 'cart', component: CartComponent },
-  { path: 'item-detail', component: ProductItemDetailComponent }
+  { path: 'item-detail', component: ProductItemDetailComponent },
+  { path: 'check-out', component: CheckOutComponent }
 ];
 
 @NgModule({
